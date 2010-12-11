cc8a0ad04bd65883a7e9cc77d051ff876fe27bc1
David Yip <yipdw@member.fsf.org>
2010-12-10 21:41:22 -0600

    Reduced indentation for Sequel::Plugins::Sunspot::InstanceMethods.

--- lib/sequel/plugins/sunspot/instance_methods.rb
+++ lib/sequel/plugins/sunspot/instance_methods.rb
@@ -1,33 +1,29 @@
-module Sequel
-  module Plugins
-    module Sunspot
-      ##
-      # Instance methods mixed into every model class that uses the Sunspot
-      # plugin.
-      #
-      # This module is meant to be used by `Sequel::Model.plugin`.
-      module InstanceMethods
-        ##
-        # Indexes a model without committing changes to the Solr index.
-        #
-        # To commit changes, follow an invocation of {#index} with
-        # `Sunspot.commit` or use {#index!} on its own.
-        #
-        # @return [void]
-        def index
-          ::Sunspot.index(self)
-        end
+module Sequel::Plugins::Sunspot
+  ##
+  # Instance methods mixed into every model class that uses the Sunspot
+  # plugin.
+  #
+  # This module is meant to be used by `Sequel::Model.plugin`.
+  module InstanceMethods
+    ##
+    # Indexes a model without committing changes to the Solr index.
+    #
+    # To commit changes, follow an invocation of {#index} with
+    # `Sunspot.commit` or use {#index!} on its own.
+    #
+    # @return [void]
+    def index
+      ::Sunspot.index(self)
+    end
 
-        ##
-        # Indexes a model and commits changes to the Solr index.
-        #
-        # If you're indexing many records at a time, it's usually more
-        # time-efficient to use {#index} on each record and then call
-        # `Sunspot.commit`.
-        def index!
-          ::Sunspot.index!(self)
-        end
-      end
+    ##
+    # Indexes a model and commits changes to the Solr index.
+    #
+    # If you're indexing many records at a time, it's usually more
+    # time-efficient to use {#index} on each record and then call
+    # `Sunspot.commit`.
+    def index!
+      ::Sunspot.index!(self)
     end
   end
 end
