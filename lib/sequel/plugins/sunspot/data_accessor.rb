53fa74fa2915629eb3c9f34462bebd1a99507034
David Yip <yipdw@member.fsf.org>
2010-12-10 21:27:17 -0600

    Filled out DataAccessor.

--- lib/sequel/plugins/sunspot/data_accessor.rb
+++ lib/sequel/plugins/sunspot/data_accessor.rb
@@ -2,5 +2,12 @@ require 'sunspot'
 
 module Sequel::Plugins::Sunspot
   class DataAccessor < ::Sunspot::Adapters::DataAccessor
+    def load(id)
+      @clazz[id]
+    end
+
+    def load_all(ids)
+      @clazz.where(@clazz.primary_key => ids)
+    end
   end
 end
--- spec/sequel/plugins/sunspot/data_accessor_spec.rb
+++ spec/sequel/plugins/sunspot/data_accessor_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 module Sequel::Plugins::Sunspot
   describe DataAccessor do
+    let(:w1) { Widget.new }
+    let(:w2) { Widget.new }
+    let(:accessor) { DataAccessor.new(Widget) }
 
+    before do
+      [w1, w2].each { |w| w.save }
+    end
+
+    describe '#load' do
+      it 'loads one record by its ID' do
+        accessor.load(w1.id).should == w1
+      end
+    end
+
+    describe '#load_all' do
+      it 'loads many records by their IDs' do
+        accessor.load_all([w1.id, w2.id]).should include(w1, w2)
+      end
+
+      it 'can be filtered further' do
+        accessor.load_all([w1.id, w2.id]).where(:id => w1.id).all.should == [w1]
+      end
+    end
   end
 end
