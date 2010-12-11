8d680c2bebe593763a18ef6759e5a991d52e3e38
David Yip <yipdw@member.fsf.org>
2010-12-11 02:11:41 -0600

    Path correction, take 2.

--- ci/env.sh
+++ ci/env.sh
@@ -2,7 +2,7 @@ set +xe
 echo ". ~/.rvm/scripts/rvm"
 set -x
 
-mydir=`dirname "$0"` && rvm use `rake -sf $mydir/init.rakefile get_rvm_version`
+rvm use `rake -sf $PWD/ci/init.rakefile get_rvm_version`
 rvm_gemset_create_on_use_flag=1 && rvm gemset use sunspot-sequel
 
 set -e
