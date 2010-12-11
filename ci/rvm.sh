dc9a9473527af1e2bb4cd44781b9945fb05ecce5
David Yip <yipdw@member.fsf.org>
2010-12-11 02:14:39 -0600

    Moved RVM loading out of the CI setup script: the CI server should handle that.
    Also renamed said script to clarify that it's intended for use for RVM.

--- ci/rvm.sh
+++ ci/rvm.sh
@@ -1,8 +1,4 @@
-set +xe
-echo ". ~/.rvm/scripts/rvm"
-set -x
-
+set +e
 rvm use `rake -sf $PWD/ci/init.rakefile get_rvm_version`
 rvm_gemset_create_on_use_flag=1 && rvm gemset use sunspot-sequel
-
 set -e
