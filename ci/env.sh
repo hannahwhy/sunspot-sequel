a10206fd6de23e1a1d885d86edd78a2b703c674c
David Yip <yipdw@member.fsf.org>
2010-12-11 02:09:38 -0600

    Corrected path to init.rakefile.

--- ci/env.sh
+++ ci/env.sh
@@ -2,7 +2,7 @@ set +xe
 echo ". ~/.rvm/scripts/rvm"
 set -x
 
-rvm use `rake -sf $PWD/init.rakefile get_rvm_version`
+mydir=`dirname "$0"` && rvm use `rake -sf $mydir/init.rakefile get_rvm_version`
 rvm_gemset_create_on_use_flag=1 && rvm gemset use sunspot-sequel
 
 set -e
